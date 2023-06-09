43f444985d54b939b97681c96bcf94003b1ceff0
soheilghanbary <soli.ghanbary@gmail.com>
2023-06-10 00:32:13 +0330

    add sign in loading

--- app/guestbook/sign-button.tsx
+++ app/guestbook/sign-button.tsx
@@ -2,15 +2,24 @@
 import { Button } from "@/components/ui/button";
 import { GithubIcon } from "lucide-react";
 import { signIn } from "next-auth/react";
+import { useState } from "react";
 
 export default function SignButton() {
+  const [isLoading, setisLoading] = useState(false)
+
+  const onSignIn = () => {
+    setisLoading(true)
+    signIn("github")
+  }
+
   return (
     <Button
-      onClick={() => signIn("github")}
+      onClick={onSignIn}
       className="mt-4"
       variant={"outline"}
+      disabled={isLoading}
     >
-      Sign in with
+      {isLoading ? 'Sign In ...' : 'Sign in with'}
       <GithubIcon className="w-5 h-5 ml-3" />
     </Button>
   );
