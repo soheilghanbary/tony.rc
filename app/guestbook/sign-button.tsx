df2525fd4f5a8b356711be61c3b6d90e83eec7f8
soheilghanbary <soli.ghanbary@gmail.com>
2023-06-10 00:40:56 +0330

    fix more

--- app/guestbook/sign-button.tsx
+++ app/guestbook/sign-button.tsx
@@ -11,15 +11,16 @@ export default function SignButton() {
     <Button
         variant={"outline"}
         disabled={isGitHubLoading}
+        className="mt-4"
         onClick={() => {
           setisGitHubLoading(true);
           signIn("github");
         }}
       >
         {isGitHubLoading ? (
-          <Loader2 className="animate-spin" />
+          <Loader2 className="animate-spin w-5 h-5 mr-2" />
         ) : (
-          <GithubIcon/>
+          <GithubIcon className="w-5 h-5 mr-2"/>
         )}{" "}
         Github
       </Button>
