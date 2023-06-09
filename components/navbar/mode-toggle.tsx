7584e460d4ac0530da437fe5652683597da15570
soheilghanbary <soli.ghanbary@gmail.com>
2023-06-10 01:56:21 +0330

    add minimal animation to mode toggle

--- components/navbar/mode-toggle.tsx
+++ components/navbar/mode-toggle.tsx
@@ -1,8 +1,10 @@
+
 "use client";
 import { cn } from "@/lib/utils";
-import { GithubIcon, MoonIcon } from "lucide-react";
+import { GithubIcon, MoonIcon, SunIcon } from "lucide-react";
 import { useTheme } from "next-themes";
 import { Button, buttonVariants } from "../ui/button";
+import { motion } from "framer-motion";
 
 const ModeToggle = () => {
   const { theme, setTheme } = useTheme();
@@ -12,17 +14,19 @@ const ModeToggle = () => {
   return (
     <div>
       <Button variant={"ghost"} onClick={toggleTheme}>
-        <MoonIcon className="w-5 h-5" />
+        {theme === 'dark' ? <motion.div animate={{ rotate: 360 }}><SunIcon className="w-5 h-5" /></motion.div> : <motion.div animate={{ rotate: 360 }}><MoonIcon className="w-5 h-5" /></motion.div>}
       </Button>
       <a
         target={"_blank"}
         href={"https://github.com/soheilghanbary"}
         className={cn(buttonVariants({ variant: "ghost" }))}
       >
-        <GithubIcon className="w-5 h-5" />
+        <motion.div animate={{ rotate: 360 }}><GithubIcon className="w-5 h-5" /></motion.div>
       </a>
     </div>
   );
 };
 
 export default ModeToggle;
+
+
