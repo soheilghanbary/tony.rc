e4c247773e1024a95f352ada4afc31c73ae8e9cd
soheilghanbary <soli.ghanbary@gmail.com>
2023-06-10 00:38:19 +0330

    fix it

--- app/guestbook/sign-button.tsx
+++ app/guestbook/sign-button.tsx
@@ -1,26 +1,27 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { GithubIcon } from "lucide-react";
+import { GithubIcon, Loader2 } from "lucide-react";
 import { signIn } from "next-auth/react";
 import { useState } from "react";
 
 export default function SignButton() {
-  const [isLoading, setisLoading] = useState(false)
-
-  const onSignIn = () => {
-    setisLoading(true)
-    signIn("github")
-  }
+  const [isGitHubLoading, setisGitHubLoading] = useState(false);
 
   return (
     <Button
-      onClick={onSignIn}
-      className="mt-4"
-      variant={"outline"}
-      disabled={isLoading}
-    >
-      {isLoading ? 'Sign In ...' : 'Sign in with'}
-      <GithubIcon className="w-5 h-5 ml-3" />
-    </Button>
+        variant={"outline"}
+        disabled={isGitHubLoading}
+        onClick={() => {
+          setisGitHubLoading(true);
+          signIn("github");
+        }}
+      >
+        {isGitHubLoading ? (
+          <Loader2 className="animate-spin" />
+        ) : (
+          <GithubIcon/>
+        )}{" "}
+        Github
+      </Button>
   );
 }
